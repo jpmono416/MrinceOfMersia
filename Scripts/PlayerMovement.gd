982bbbf788b40b9f44cb5886cb0b875ec2f2d306
unknown <rasurap666@gmail.com>
2024-07-08 23:40:33 +0200

    Commit de  prueba

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,10 +1,7 @@
-extends CharacterBody2D
-
+# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var animationPlayer = $AnimationPlayer
-const SPEED = 220
-const JUMP_VELOCITY = -515
-
+onready var animationPlayer = $AnimationPlayer
+ 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -12,43 +9,34 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		animationPlayer.play("Jump")
 		velocity.y = JUMP_VELOCITY
 
-
-	if !Input.get_axis("ui_left", "ui_right"):
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	# Get input for movement
-	if Input.is_action_pressed("move_right"):
-		velocity.x = SPEED
-	if Input.is_action_pressed("move_left"):
-		velocity.x = -SPEED
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += SPEED
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= SPEED
 	if Input.is_action_pressed("ui_down"):
-		velocity.y = SPEED
+		velocity.y += SPEED
 	if Input.is_action_pressed("ui_up"):
-		velocity.y = SPEED
+		velocity.y -= SPEED
+
+	# Apply movement
+	velocity = move_and_slide(velocity)
 
 	# Handle animation changes
 	if velocity.length() > 0:
 		if abs(velocity.x) > abs(velocity.y):
-			if animationPlayer.current_animation != "Run" && is_on_floor():
-				animationPlayer.play("Run")
+			if animationPlayer.current_animation != "run":
+				animationPlayer.play("run")
 		else:
-			if velocity.y > 0:
-				if animationPlayer.current_animation != "Fall":
-					animationPlayer.play("Fall")
-			else:
-				if animationPlayer.current_animation != "Jump":
-					animationPlayer.play("Jump")
+			if animationPlayer.current_animation != "run":
+				animationPlayer.play("run")
 
 		if velocity.x < 0:
-			$Sprite2D.flip_h = true # Flip sprite for left movement
+			$Sprite.flip_h = true # Flip sprite for left movement
 		elif velocity.x > 0:
-			$Sprite2D.flip_h = false # Normal sprite for right movement
+			$Sprite.flip_h = false # Normal sprite for right movement
 	else:
-		if animationPlayer.current_animation != "Idle":
-			animationPlayer.play("Idle")
-
-	# Apply movement
-	move_and_slide()
+		if animationPlayer.current_animation != "idle":
+			animationPlayer.play("idle")
